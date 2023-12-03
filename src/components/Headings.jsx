75d7dc4638b59702f7ca52cba40144520331534e
kareem roqib <kareemolakunleh@gmail.com>
2023-12-03 12:08:40 +0100

    fix : fix the overflow bug

--- src/components/Headings.jsx
+++ src/components/Headings.jsx
@@ -1,19 +1,19 @@
 import PropTypes from "prop-types";
 function Headings({ heading, subHeading }) {
-  return (
-    <header className="text-center mt-[12.12rem]">
-      <h1 className="text-8xl text-white uppercase font-gothic font-normal gothic">
-        {heading}
-      </h1>
-      <p className="text-white font-normal text-center uppercase font-spartan text-[2rem] md:text-4xl spartan">
-        {subHeading}
-      </p>
-    </header>
-  );
+	return (
+		<header className="text-center mt-[12.12rem]">
+			<h1 className="text-6xl lg:text-8xl text-white uppercase font-gothic font-normal gothic">
+				{heading}
+			</h1>
+			<p className="text-white font-normal text-center uppercase font-spartan text-2xl md:text-4xl spartan">
+				{subHeading}
+			</p>
+		</header>
+	);
 }
 
 Headings.propTypes = {
-  heading: PropTypes.string,
-  subHeading: PropTypes.string,
+	heading: PropTypes.string,
+	subHeading: PropTypes.string,
 };
 export default Headings;
--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -2,16 +2,16 @@ import PropTypes from "prop-types";
 import Headings from "./Headings";
 
 function TopSection({ heading, subHeading }) {
-  return (
-    <section className="bg-black w-full px-[2.56rem]  md:px-20 lg:px-[7.5rem] pb-[12rem] pt-1">
-      <Headings heading={heading} subHeading={subHeading} />
-    </section>
-  );
+	return (
+		<section className="bg-black w-full px-4  md:px-20 lg:px-[7.5rem] pb-[12rem] pt-1">
+			<Headings heading={heading} subHeading={subHeading} />
+		</section>
+	);
 }
 
 TopSection.propTypes = {
-  heading: PropTypes.string,
-  subHeading: PropTypes.string,
+	heading: PropTypes.string,
+	subHeading: PropTypes.string,
 };
 
 export default TopSection;
