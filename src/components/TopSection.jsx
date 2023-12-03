d33c4101ef86a3c49455a07d611d10cb75b2ed0c
kareem roqib <kareemolakunleh@gmail.com>
2023-12-03 12:17:06 +0100

    fix : fix the overflow bug

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -3,7 +3,7 @@ import Headings from "./Headings";
 
 function TopSection({ heading, subHeading }) {
 	return (
-		<section className="bg-black w-full px-4  md:px-20 lg:px-[7.5rem] pb-[12rem] pt-1">
+		<section className="bg-black w-full px-4 min-h-screen  md:px-20 lg:px-[7.5rem] pb-[12rem] pt-1">
 			<Headings heading={heading} subHeading={subHeading} />
 		</section>
 	);
